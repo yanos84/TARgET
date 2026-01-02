8dfd2435637c1b7909bcd78c3fc499516daf8b82
yanos84 <gyounes84@gmail.com>
2026-01-02 10:58:35 +0000

    Creating cli module for making shell commands

--- cli/main.py
+++ cli/main.py
@@ -1,7 +1,8 @@
 import argparse
-from engine.fta.drawing.rankedFtaDraw import draw_ranked_fta as RankedFtaDrawer
-from engine.fta.acceptors.rankedAcceptor import RankedAcceptor
-from engine.fta.random.ranked_fta_generator import RandomRankedFtaGenerator
+from engine.fta.drawing.rankedFtaDraw import draw_ranked_fta
+#from engine.fta.acceptors.rankedAcceptor import RankedAcceptor
+#from engine.fta.random.ranked_fta_generator import RandomRankedFtaGenerator
+from engine.utils.rankedFta_xml_import import load_fta_from_xml
 
 def main():
     parser = argparse.ArgumentParser(
@@ -16,4 +17,14 @@ def main():
         "draw",
         help="Generate and draw a random ranked finite tree automaton."
     )
-    #draw_parser.add_argument(
+    draw_parser.add_argument("fta_file")
+    draw_parser.add_argument("-o", "--output", default="fta.png", help="Output image file name.")
+    args = parser.parse_args()
+
+    if args.command == "draw":
+        fta =load_fta_from_xml(args.fta_file)
+        dot = draw_ranked_fta(fta)
+        dot.render("fta1", format="png", cleanup=True) 
+
+if __name__ == "__main__":
+    main()
