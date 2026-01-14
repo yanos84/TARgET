65eaf5d1642c9eed021e84b7f5cae00908b4b455
yanos84 <gyounes84@gmail.com>
2026-01-14 12:19:01 +0000

    Adding complement algorithm for all bottom-up fta to engine

---
+++ engine/fta/complement/complement.py
@@ -0,0 +1,42 @@
+from fta.abst_fta import Fta
+from fta.state import State
+from typing import List
+from core.symbol import Symbol, Ranked_Symbol
+import copy
+
+class Complement():
+
+    def __init__(self, fta: Fta) -> None:
+        self.fta = copy.deepcopy(fta)
+    
+    def compute_complement(self) -> Fta:
+        # Placeholder for complement computation logic
+        # This should return a new Fta instance representing the complement of self.fta
+        alphabet =getattr(self.fta, 'alphabet', None)
+        if alphabet is not None and isinstance(alphabet, list):
+            for sym in alphabet:
+                if not isinstance(sym, Symbol):
+                    raise TypeError("Alphabet must contain only Symbol instances.")
+            for state in self.fta.states_list:
+                state.is_Final = not state.is_Final
+            for r in self.fta.transitions:
+                for input in r.inputs:
+                    input.is_Final = not input.is_Final
+                r.output.is_Final = not r.output.is_Final
+            return self.fta
+        else:
+            raise TypeError("Alphabet must be a list of Symbol instances.")
+
+
+## Example usage
+if __name__ == "__main__":
+    from fta.rankedfta import ranked_Fta
+    s1 = State(name="q0", final=True)
+    s2 = State(name="q1", final=False)
+    states = [s1, s2]
+    fta = ranked_Fta(fta_name="example_fta", fta_states=states, alphabet=[Ranked_Symbol(name="a", rank=0)], transitions=[])
+
+    complement_calculator = Complement(fta)
+    complement_fta = complement_calculator.compute_complement()  
+    print("Original FTA:",fta) 
+    print("Complement FTA:",complement_fta)
